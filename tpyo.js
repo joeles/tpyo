5bc7d4c30d14f5c5cdb04859b1307e5ed543518a
Joel Escobar <joel.escobar@refinery29.com>
2015-01-25 05:51:29 -0600

    big refactor added options removed weird css blink code expanded keyboard to include uppercase and 1 extra key per row

--- tpyo.js
+++ tpyo.js
@@ -3,94 +3,108 @@
 
 var tpyo = (function() {
 
-  var element, text, index, options, timeout;
-
-  function typo() {
-
-    var keyboard = 'qwertyuiopasdfghjkl;zxcvbnm,./',
-        key_index = keyboard.indexOf(text[index]),
-        offsets = [-10, -9, -1, 1, 9, 10],
-        typo_char = keyboard[key_index + offsets[rand(0, 5)]] || text[index];
-
-    // semi-colons can be party of an html entity leave them alone
-    if (text[index] != ';' && key_index > -1) {
-      element.innerHTML += typo_char;
-      timeout = setTimeout(backspace, options.speed + rand(0, 100));
-    } else {
-      timeout = setTimeout(keypress, options.speed + rand(0, 100));
-    }
-  }
-
-  function backspace() {
-
-    element.innerHTML = element.innerHTML.replace(/.$/g, '');
-
-    timeout = setTimeout(keypress, options.speed + rand(0, 100));
-  }
-
-  function keypress() {
-
-    element.innerHTML += text[index++];
-
-    if (index < text.length) {
-      if ((index % rand(5, 20)) === 0) {
-        timeout = setTimeout(typo, options.speed + rand(0, 100));
-      } else {
-        timeout = setTimeout(keypress, options.speed + rand(0, 100));
-      }
-    } else {
-      document.querySelector('.typo-cursor').className += ' typo-blink';
-    }
-  }
+  var options = {
+        speed: 150,
+        speed_variance: 250,
+        blink_speed: 500
+      },
+      buffer,
+      cursor,
+      text,
+      index,
+      timeout;
 
   function type(selector, txt, opts) {
 
-    html_css(selector);
+    html(selector);
 
     text = txt;
-    options = opts;
     index = 0;
+
+    for (var i in opts) {
+      options[i] = opts[i];
+    }
 
     start();
   }
 
   function start() {
-    timeout = setTimeout(keypress, options.speed);
+
+    clearTimeout(timeout);
+
+    next(keypress);
   }
 
   function stop() {
+
     clearTimeout(timeout);
+
+    next(blink);
+  }
+
+  function keypress() {
+
+    buffer.innerHTML += text[index++];
+
+    if (index < text.length) {
+      if ((index % rand(5, 20)) === 0) {
+        next(typo);
+      } else {
+        next(keypress);
+      }
+    } else {
+      stop();
+    }
+  }
+
+  function typo() {
+
+    var keyboard  = "1234567890-qwertyuiop[asdfghjkl;'zxcvbnm,./"
+                  + '!@#$%^&*()_QWERTYUIOP{ASDFGHJKL:"ZXCVBNM<>?',
+        key_index = keyboard.indexOf(text[index]),
+        offsets = [-11, -10, -1, 1, 10, 11],
+        typo_char = keyboard[key_index + offsets[rand(0, (offsets.length - 1))]] || text[index];
+
+    // semi-colons can be part of an html entity leave them alone
+    if (text[index] != ';' && key_index > -1) {
+      buffer.innerHTML += typo_char;
+      next(backspace);
+    } else {
+      next(keypress);
+    }
+  }
+
+  function backspace() {
+
+    buffer.innerHTML = buffer.innerHTML.replace(/.$/g, '');
+
+    next(keypress);
+  }
+
+  function next(func) {
+
+    timeout = setTimeout(func, options.speed + rand(0, options.speed_variance));
+  }
+
+  function blink() {
+
+    cursor.innerHTML = (cursor.innerHTML.length)
+      ? ''
+      : '|';
+
+    timeout = setTimeout(blink, options.blink_speed);
   }
 
   function rand(min, max) {
+
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  function html_css(selector) {
+  function html(selector) {
 
-    var css = '.typo-cursor { opacity: 1; font-weight: 100; } .typo-blink { ',
-        style = style = document.createElement('style'),
-        vendors = ['-webkit-', '-moz-', '-ms-', '-o-', ''];
-
-    for (var v in vendors)
-      css += vendors[v] + 'animation: blink 1s infinite; '
-
-    css += 'margin-left: -5px; } ';
-
-    vendors.pop()
-    vendors.unshift('-');
-
-    for (v in vendors)
-      css += '@' + vendors[v] + 'keyframes blink { 0% { opacity:1; } 50% { opacity:0; } 100% { opacity:1; } } '
-
-    css += '}';
-
-    style.innerHTML = css;
-
-    document.body.appendChild(style);
-
-    document.querySelector(selector).innerHTML = '<span class="typo-text"></span><span class="typo-cursor">|</span>';
-    element = document.querySelector('.typo-text');
+    document.querySelector(selector).innerHTML = '<span class="text"></span><b class="cursor">|</b>';
+    buffer = document.querySelector(selector + ' .text');
+    cursor = document.querySelector(selector + ' .cursor');
   }
 
   return {
